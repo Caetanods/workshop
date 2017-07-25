50c3bc0d396136711485ef871bd53336d93e69ab
daniel <caetanods1@gmail.com>
2017-07-24 21:29:58 -0300

    Update tutorial.

--- Slides/Work_5/tutorial_5.R
+++ Slides/Work_5/tutorial_5.R
@@ -1,19 +1,16 @@
-## Nesse tutorial vamos contruir passo a passo uma busca Bayesiana usando o algoritmo de
-##    Metropolis.
-## O fluxograma mostrado em aula pode ser uma boa referência para acompanhar cada um
-##   dos passos.
+## Nesse tutorial vamos construir passo a passo uma busca heuristica Bayesiana usando o 
+##    algoritmo de Metropolis. O fluxograma mostrado em aula pode ser uma boa referência
+##    para acompanhar cada um dos passos.
 
 ## Nesse tutorial vamos aplicar o MCMC em um modelo de Brownian Motion. Podemos
 ##   facilmente mudar a função de verossimilhança e ajustar os parâmetros, priors e
-##   distribuições de propostas para implementar uma busca de MCMC de outro modelo.
+##   distribuições de propostas para implementar uma busca de MCMC de outro modelo qualquer.
 
 ## Vou aproveitar para fazer comentários sobre a implementação da análise. Espero que
 ##   ajude a implementar seu próprio MCMC no futuro.
 
-## Dica 1: Implemente cada passo do MCMC em funções separadas, dessa forma é mais fácil
-##   modificar e corrigir erros.
-
-## Distribuição de proposta:
+## Distribuição de proposta. Essa função pega um valor continuo e usa uma distribuição uniforme
+##   para gerar um valor de proposta.
 sliding.window <- function(x, w){
   ## Sliding window proposal for unbounded trait.
   ## x = the current value.
@@ -22,7 +19,7 @@ sliding.window <- function(x, w){
   return(y)
 }
 
-## Função de verossimilhança:
+## Função de verossimilhança (mesma do tutorial anterior):
 singleBML <- function(phy, x, sigsq, mean){
   ## phy = filogenia.
   ## x = dados.
@@ -53,18 +50,28 @@ data <- sim.char(phy = phy, par = 0.5, nsim = 1, model = "BM", root = 10)[,,1]
 ## Distribuições a priori:
 ## Vou definir um prior exponencial para a taxa e uma normal para o valor da raíz.
 ## Antes mesmo de observar os dados eu sei que um bom chute para o valor da raíz é
-##    a média dos valores das espécies, mas vamos deixar uma grande variância. Eu
-##    sei também que a taxa é geralmente um número pequeno, muitas vezes entre 0 e 1.
+##    a média dos valores das espécies. Vamos utilizar um valor alto para a variância
+##    dessa forma a densidade do prior não vai estar toda concentrada em uma região restria.
+## Eu sei também que a taxa é geralmente um número pequeno, muitas vezes entre 0 e 1. Então,
+##    valores muito grandes são muito menos prováveis. Sendo assim, a distribuição exponencial
+##    é perfeita para esse parâmetro.
+
+## DICA: Repita essa análise com priors uniformes para a taxa e o valor da raiz. Compare a
+##    posterior de explore o efeito dos priors. Tome cuidado para escolher o minimo e máximo
+##    do prior!
+
 prior.rate <- function(x){
+  ## Função retorna a probabilidade prior da taxa.
   dexp(x = x, rate = 0.5, log = TRUE)
 }
 mean.data <- mean(data)
 prior.root <- function(x){
+  ## Função retorna a probabilidade prior do valor de root.
   dnorm(x = x, mean = mean.data, sd = 10, log = TRUE)
 }
 
 ## Valores iniciais.
-## Vamos usar um valor inicial para os parâmetros tirado da distribuição dos priors.
+## Vamos usar um valor inicial para os parâmetros gerado da distribuição dos priors.
 initial.rate <- rexp(n = 1, rate = 0.5)
 initial.root <- rnorm(n=1, mean = mean.data, sd = 10)
 
@@ -74,12 +81,18 @@ initial.root <- rnorm(n=1, mean = mean.data, sd = 10)
 initial.rate; initial.root
 
 ## Update do parâmetro:
-## Qual parâmetro vamos propor um novo valor? Podemos atualizar os dois ou um por vez.
+## Para qual parâmetro vamos propor um novo valor? Podemos atualizar os dois de um só vez
+##    ou um por vez. O MCMC funcionaria sem problemas em ambos os casos.
 ## Vamos atualizar um parâmetro por vez. Vamos escolher um dos parãmetros. Para tal,
-##    vamos sortear usando uma probabilidade simétrica.
+##    vamos sortear usando uma certa probabilidade. A frequência com que cada parâmetro do
+##    modelo sofre update não precisa ser sempre a mesma. É normal focar a busca em alguns
+##    parâmetros mais do que em outros. Por exemplo, você pode tentar fazer updates mais
+##    frequentes para a taxa do modelo BM (rate) do que para o valor da raiz (root).
 par.update <- sample(x = c("rate","root"), size = 1, prob = c(0.5,0.5))
 
-## Agora um if / else para fazer a proposta de cada um dos parâmetros:
+## Agora um if / else para fazer a proposta de cada um dos parâmetros.
+## Essa estrutura é necessária pois somente um dos parâmetros sofre update a cada geração do
+##    MCMC.
 if(par.update == "root"){
   prop.root <- sliding.window(x = initial.root, w = 0.5)
 } else {
@@ -88,7 +101,7 @@ if(par.update == "root"){
 
 ## Agora temos um valor de proposta para um dos parâmetros.
 ## Vamos então aceitar ou rejeitar. Novamente vamos usar um if / else para trabalhar
-##   com o parâmetro correto.
+##   com o parâmetro escolhido.
 if(par.update == "root"){
   ## Primeiro a odds ratio.
   lik.initial.root <- singleBML(phy=phy, x=data, sigsq=initial.rate, mean=initial.root)
@@ -108,7 +121,7 @@ if(par.update == "root"){
   r.rate <- ll + pp ## ratio of log(lik) + ratio of log(prior)
 }
 
-## Agora temo a odds ratio. Com ela podemos fazer o passo de aceitar ou rejeitar.
+## Agora temos a odds ratio calculada. Com ela podemos fazer o passo de aceitar ou rejeitar.
 ## Novamente vamos fazer um if / else para ajustar de acordo com o parâmetro.
 
 if(par.update == "root"){
@@ -153,12 +166,18 @@ ifelse(test = par.update == "root", yes = paste("root", prop.root), no = paste("
 ## Próximo estado da cadeia, após aceitar ou rejeitar a proposta:
 next.root; next.rate
 
+## Se o valor proposta for aceito, então a próxima geração terá o valor da proposta como o
+##      valor atual para aquele parametro. Do contrário, se a proposta foi rejeitada o
+##      valor anterior dos parameters será mantido como o valor atual.
+
 ## Esse foi somente um passo da cadeia de MCMC usando o algoritmo de Metropolis.
-## Certifique-se de que entendeu cada um dos passos acima.
+## Certifique-se de que entendeu cada um dos passos acima!!
+
 ## Relacione o que foi feito aqui com o fluxograma mostrado na aula.
 
 ## Note que o passo de aceitar ou rejeitar não é exatamente igual ao que foi mostrado
 ##   na aula. Por quê?
+
 ## A forma que foi implementado aqui (em pseudo-código):
 # if(exp(r) > runif(1)){
 # # Aceite.
@@ -167,20 +186,25 @@ next.root; next.rate
 # }
 
 ## Qual a relação desta implementação com o que foi mostrado em aula?
+## Porque usamos 'exp(r)' e não somente 'r'?
 
 ## No próximo tutorial vamos construir uma função completa que fará a análise de
 ##    MCMC. Essa função terá os mesmo passos que foram mostrados acima. As diferenças
 ##    serão somente uma questão de implementação, visto que o código acima não
 ##    está preparado para rodar milhares de vezes, como é necessário no MCMC.
 
-## Como você faria para rodar esses passos milhares de vezes?
+## Como você faria para rodar esses passos milhares de vezes? Quais seriam os cuidados que você
+##    tomaria no seu programa?
 
-## Quais informações (objetos/valores) devemos salvar ao longo das gerações?
+## Quais informações (objetos/valores) devemos guardar a cada uma das gerações?
+## Quais informações o programa que você usa que faz inferência Bayesiana guarda para cada
+##    geração?
 
 ## Sabemos que variância não aceita valores negativos. Existe algum problema com esse
-##   esquema de propostas de novos valores? Qual é o problema?
+##   esquema de propostas de novos valores usado aqui? Qual é o problema?
 
 ## DESAFIO 1: Tente inventar uma maneira de não gerar ou não aceitar valores
 ##   negativos de rate.
+
 ## DESAFIO 2: Use o exemplo acima para fazer mais duas gerações de MCMC. Guarde os
 ##    resultados de cada uma das gerações.
