3095d34d8a03606c427a297cdf883e496bc2679a
daniel <caetanods1@gmail.com>
2017-07-26 09:24:21 -0300

    update tutorials

--- Slides/Work_7/tutorial_7.R
+++ Slides/Work_7/tutorial_7.R
@@ -1,8 +1,7 @@
 ## No tutorial anterior completamos uma análise usando o algoritmo de Metropolis.
 ## Nesse tutorial vamos fazer análises da posterior.
-## Caso tenha resultados de uma análise de alguma outra análise de MCMC você pode
-##   usar seus próprios dados para rodar esse tutorial. Note que a formação dos
-##   objetos e interpretação dos parãmetros fica por sua conta! ;)
+## Caso tenha resultados de uma análise MCMC de algum outro modelo de sua escolha, tente
+##   utilizar com esse tutorial.
 
 ## Para nossas análises vamos utilizar o pacote 'coda'.
 if(!require(coda)) {install.packages("coda"); library(coda)}
@@ -12,12 +11,17 @@ if(!require(coda)) {install.packages("coda"); library(coda)}
 
 ## Primeiro precisamos carregar nossa cadeia.
 ## Lembre-se de mudar o diretório abaixo para onde os seus arquivos estão:
+## Esse é o caminho para o diretório no meu computador:
 setwd("~/Documents/Academicos/Harmon_Lab/MCMC - Workshop - Brasil/workshop/Slides/Work_7")
 mcmc.1 <- readRDS(file = "../Work_6/mcmc.BM.1.rds")
 mcmc.2 <- readRDS(file = "../Work_6/mcmc.BM.2.rds")
 mcmc.3 <- readRDS(file = "../Work_6/mcmc.BM.3.rds")
 
 ## Agora criamos um objeto da classe 'mcmc'.
+
+## Veja a pagina de ajuda dessa função:
+help( mcmc )
+
 ## Note que a função tem opções para retirada do burn-in (informando a geração de
 ##   início) e aplicação do thinning. Mas, por enquanto, vamos usar a cadeia toda.
 mcmc.1 <- mcmc(mcmc.1)
@@ -29,18 +33,23 @@ mcmc.3 <- mcmc(mcmc.3)
 help(mcmc.list)
 mcmc <- mcmc.list(mcmc.1, mcmc.2, mcmc.3)
 
+## Somente podemos combinar as cadeias de MCMC pois estamos trabalhando com o mesmo
+##   modelo e com o mesmo conjunto de dados. Portanto, cada uma dessas cadeias são
+##   resultados de uma mesma análise.
+
 ## Primeiramente podemos ver o 'summary' das nossas cadeias:
 summary(mcmc)
-## Nesse summary podemos ver uma quantidade importante. O 95% HPD, a região de 95%
-##   de maior densidade (i.e., probabilidade) dos parâmetros. Veja, por exemplo,
-##   que o valor de 'root' (valor da raíz da filogenia) varia entre 9.41 e 11.47 no
-##   95% HPD. Mas ainda temos outras análises a fazer.
+## Nesse summary podemos ver uma quantidade importante. O 95% HPD (highest posterior density),
+##   a região de 95% de maior densidade (i.e., probabilidade posterior) dos parâmetros.
+##   Veja, por exemplo, que o valor de 'root' (valor da raíz da filogenia) varia entre
+##   9.41 e 11.47 no 95% HPD. Mas ainda temos outras análises a fazer.
 
 ## Antes de mais nada vamos retirar o burn-in. Para tal vamos observar o comportamento
 ##   das cadeias usando plots.
 ## Essa função vai produzir mais de um plot. Então talvez você terá que mover entre
 ##   as janelas dependendo do programa que esteja usando.
-traceplot(x = mcmc)
+plot(x = mcmc, trace = TRUE, density = FALSE)
+plot(x = mcmc, trace = FALSE, density = TRUE)
 
 ## Note que as cadeias são colocadas no mesmo gráfico. Isso facilita a observação da
 ##   convergência. Podemos visualmente verificar se as diferentes cadeias estão
@@ -51,7 +60,8 @@ traceplot(x = mcmc)
 ## Pelos gráficos parece que a partir da geração '2000' já temos uma estabilização.
 ## Vamos retirar as gerações de 1:2000 e plotar novamente:
 post.mcmc <- window(mcmc, start=2000, end=10000)
-traceplot(post.mcmc)
+plot(x = post.mcmc, trace = TRUE, density = FALSE)
+plot(x = post.mcmc, trace = FALSE, density = TRUE)
 
 ## Já está com uma cara melhor, não?
 
@@ -61,26 +71,38 @@ summary(post.mcmc)
 ## Agora que retiramos o burn-in vamos fazer alguns diagnósticos de convergência.
 ## Vamos usar o Gelman and Rubin R que verifica se a variação entre cadeias é menor
 ##   que a variação dentro das cadeias.
-## Vamos colocar 'multivariate' pois queremos avaliar cada um dos parâmetros
-##  separadamente. Veja outras opções no help.
+## Vamos colocar 'multivariate=FALSE' pois queremos avaliar cada um dos parâmetros
+##   independentemente. Ou seja, não estamos considerando a correlação entre esses
+##   parâmetros.
+##   Veja outras opções no pagina de ajuda dessa função.
+
 help(gelman.diag)
+
 gelman.diag(x = post.mcmc, transform = FALSE, autoburnin = FALSE, multivariate = FALSE)
 
-## Queremos que o resultado seja o mais próximo possível de '1.00'. E... BINGO!
+## Queremos que o resultado seja o mais próximo possível de '1.00'.
 
-## OK. Retiramos o burn-in e sabemos que as cadeias convergiram. O que mais podemos
+## As vezes esse teste vai resultar em um valor próximo de 1. Por exemplo, 1.05 ou até mesmo
+##  valores maiores, próximos de 1.1. Nesse caso, sabemos que as cadeias estão quase convergindo
+##  completamente. Sabemos que está tudo funcionando na nossa análise. Podemos aumentar o número
+##  de gerações para conseguir uma melhor amostragem da posterior.
+
+## OK. Retiramos o burn-in e sabemos que as cadeias convergiram. No que mais podemos
 ##  estar interessados?
+
 ## Antes de olhar diretamente para a distribuição posterior dos parâmetros vamos 
 ##  verificar a autocorrelação e calcular o tamanho efetivo da amostragem (ESS).
-## 'realtive = FALSE' pois não aplicamos o thinning na cadeia.
+## Note que usamos 'relative = FALSE', pois não aplicamos o thinning na cadeia.
 ## Essa função mostra tanto a autocorrelação entre parâmetros como de um parâmetro só.
 ## O valor 'Lag x' é o número de gerações que foram puladas para calcular a
 ##   autocorrelação. 'Lag 5' significa que foram somente avaliadas gerações de 5 em 5.
+
 autocorr(x = post.mcmc, relative = FALSE)
 
 ## A autocorrelação é bastante alta até um Lag de 10. Indicando que para diminuir
 ##   esse valor de autocorrelação deveriamos descartar valores da cadeia e reter somente
 ##   uma em cada 10 gerações. Não vamos fazer esse passo agora.
+## Esse procedimento é chamado de 'thinning' da cadeia de MCMC.
 
 ## Qual o número de Effective Sample Size?
 effectiveSize(x = post.mcmc)
@@ -90,28 +112,27 @@ effectiveSize(x = post.mcmc)
 ##   gerações de MCMC para aumentar o número de ESS. Muito provavelmente ~ 400
 ##   pontos independentes de amostragem é o bastante para se ter uma idéia da
 ##   distribuição posterior dos parâmetros.
+## Claro, diferentes casos podem necessitar diferentes numeros de ESS. No programa
+##   BEAST, por exemplo, é recomendado um número mínimo de 200.
 
+par(mfrow = c(2,2))
 densplot(x = post.mcmc)
+par(mfrow = c(1,2))
+
+plot(x = post.mcmc, trace = FALSE, density = TRUE)
 
 ## Veja a densidade para 'root' a média do processo de Brownian Motion (valor da raíz)
 ##   e 'rate' a variância do processo (ou taxa).
 ## Essa distribuição é chamada de posterior distribution e é nosso resultado final.
-## Agora podemos ustilizar tal distribuição para fazer conclusões sobre nosso modelo.
+## Agora podemos utilizar tal distribuição para fazer conclusões sobre nosso modelo.
 
 ## Um ponto de discussão importante que é difícil de ser abordado é "Quantas
 ##  gerações de MCMC eu preciso rodar?". Uma prática comum é verificar artigos com
 ##  problemas similares e ver quanto é geralmente aceito. Para inferência filogenética
 ##  , por exemplo, geralmente são utilizados valores na casa dos milhões. No entanto,
 ##  problemas com modelos de 2 a 5 parâmetros acabam ficando na cada dos milhares.
-## Existe um método que tenta estimar quantas gerações seriam necessárias para
-##  atingir um certo número de samples. Esse método é bom para verificar
-##  cadeias 'teste'.
-
-raftery.diag(data = mcmc.1)
-
-## DESAFIO: Veja que o número total de gerações para estimar o parâmetro 'rate'
-##  é maior do que rodamos. Tente aumentar o número de gerações. Qual a diferença
-##  na estimativa de convergência, ESS, e do 95% HPD?
+## Na prática, temos que rodar análises de MCMC com diferentes números de gerações e
+##  observar o comportamento da estimativa.
 
 ## Nesse tutorial mostrei como acessar convergência e verificar burn-in quando
 ##  temos mais de uma cadeia de MCMC começando de pontos iniciais diferentes.
@@ -135,4 +156,4 @@ help(geweke.diag)
 help(heidel.diag)
 
 ## DESAFIO: Tente rodar esses diagnósticos e verificar a convergência das cadeias
-##  uma a uma. Descreva as vantagens e desvantagens de cada diagnóstico.
+##  uma a uma.
